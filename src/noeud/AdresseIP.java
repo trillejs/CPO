16ad686621a8384d1856665b1fd8ec60cff2f3ca
salimtaabani <salimtaabani@yahoo.com>
2016-06-02 08:51:50 +0200

    simulateur v1

--- src/noeud/AdresseIP.java
+++ src/noeud/AdresseIP.java
@@ -27,8 +27,15 @@ public class AdresseIP {
     public int[] getAdresse() {
         return adresse;
     }
+    
+    /**
+	 * @param adresse the adresse to set
+	 */
+	public void setAdresse(int[] adresse) {
+		this.adresse = adresse;
+	}
 
-    @Override
+	@Override
     public String toString() {
         String ip = new String();
         for (int i = 0; i < 4; i++) {
