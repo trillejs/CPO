7d411ee6f97b352e7e8fe64428beadc6fca0e4f3
trillejs <trillejs@gmail.com>
2016-06-02 09:27:08 +0200

    Ajout paquetages tests

---
+++ src/tests/mobilite/ModeleMobilite.java
@@ -0,0 +1,14 @@
+package tests.mobilite;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ModeleMobilite {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
