46dd289441bce8d793ee06f068f13215f4190eaa
Florian Postic <florian.postic@laposte.net>
2016-06-07 15:12:04 +0200

    utilisation du type ISimulateur au lieu de Simulateur

--- src/simulateur/IOperation.java
+++ src/simulateur/IOperation.java
@@ -5,8 +5,9 @@ public interface IOperation {
 	
 	/**executer
 	 * Execute les intructions composant l'opération
+	 *
 	 * @param simulateur - Simulateur : Simulateur
 	 * @param date - int : Date d'execution de l'opération
 	 */
-	public void executer(Simulateur simulateur, int date);
+	public void executer(ISimulateur simulateur, int date);
 }
