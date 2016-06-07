0edaccfcb0987cfcf446eafbd319ca1dfacf2a94
Florian Postic <florian.postic@laposte.net>
2016-06-07 21:46:49 +0200

    Ajout des methodes simuler et simulerPas

--- src/simulateur/ISimulateur.java
+++ src/simulateur/ISimulateur.java
@@ -1,5 +1,7 @@
 package simulateur;
 
+import java.util.Queue;
+
 public interface ISimulateur {
 
     /**enregistrer
@@ -10,7 +12,7 @@ public interface ISimulateur {
      * Postcondition : L'évenement appartient à la file
      *                  && La file d'attente n'est pas vide
      */
-    public void enregistrer(IEvenement evenement);
+    void enregistrer(IEvenement evenement);
 
     /**
      * avancer
@@ -21,5 +23,55 @@ public interface ISimulateur {
      * Précondition : La file d'attente n'est pas vide
      * Postcondition : /
      */
-    public void avancer();
+    void avancer();
+
+    /**simuler
+     * Lance la simulation jusqu'à ce que la simulation ai durée tFin interval
+     * de temps
+     */
+    void simuler();
+
+    /**simulerPas
+     * Lance la simulation jusqu'à ce que le temps de simulation courant
+     * atteigne temps courant + nbPas
+     * @param nbPas - int : Nombre de pas
+     */
+    void simulerPas(int nbPas);
+
+    /**getFileAttente
+     * retourne l'attribut fileAttente de l'objet de type Simulateur
+     *
+     * File des évenements en attente d'être executés
+     *
+     * @return Queue<IEvenement>
+     */
+    public Queue<IEvenement> getFileAttente();
+
+
+    /**getFileFini
+     * retourne l'attribut fileFini de l'objet de type Simulateur
+     *
+     * File des évenements terminés
+     *
+     * @return Queue<IEvenement>
+     */
+    public Queue<IEvenement> getFileFini();
+
+    /**gettCourant
+     * retourne l'attribut tCourant de l'objet de type Simulateur
+     *
+     * Compteur de temps virtuel
+     *
+     * @return int
+     */
+    public int gettCourant();
+
+    /**gettFin
+     * retourne l'attribut tFin de l'objet de type Simulateur
+     *
+     * Instant auquel se termine la simulation
+     *
+     * @return int
+     */
+    public int gettFin();
 }
