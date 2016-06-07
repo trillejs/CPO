f969dfab7e14e62f73cd892098dd5e814c1a904f
Florian Postic <florian.postic@laposte.net>
2016-06-07 21:48:33 +0200

    Suppression des Evenements spécialisés pour transformer cette classe abstraite en classe classique

--- src/simulateur/Evenement.java
+++ src/simulateur/Evenement.java
@@ -2,12 +2,26 @@ package simulateur;
 
 import java.util.Observable;
 
-public abstract class Evenement extends Observable implements IEvenement{
+/**Evenement
+ * Evenement générique, la spécialisation se fait par l'attribut opération
+ * @author Florian Postic
+ */
+public class Evenement extends Observable implements IEvenement{
 	
 	/** Date à laquelle l'évènement devra se produire */
 	protected int tExec;
 	/** Opération à executer lorsque l'évènement survient */
 	protected IOperation operation;
+
+	/**Evenement
+	 *
+	 * @param tExec - int : Date à laquelle l'évènement devra se produire
+	 * @param operation - IOperation : Opération à executer lorsque l'évènement survient
+	 */
+	public Evenement(int tExec, IOperation operation) {
+		this.tExec = tExec;
+		this.operation = operation;
+	}
 
 	/**seProduire
      * Lance l'opération associé à l'évenement et notifie le simulateur
@@ -15,16 +29,18 @@ public abstract class Evenement extends Observable implements IEvenement{
      * @param simulateur - ISimulateur : Simulateur exécutant l'évènement
      */
 	@Override
-	public abstract void seProduire(ISimulateur simulateur);
+	public void seProduire(ISimulateur simulateur){
+		operation.executer(simulateur, tExec);
+	}
 
 
 	/**getTExec
-	 * retourne l'attribut TExec de l'objet de type Evenement
-	 *
-	 * Date à laquelle l'évènement devra se produire
-	 *
-	 * @return int
-	 */
+     * retourne l'attribut TExec de l'objet de type Evenement
+     *
+     * Date à laquelle l'évènement devra se produire
+     *
+     * @return int
+     */
 	public int getTExec() {
 		return tExec;
 	}
