94143755605a9ab33ad93966700bb0b634167133
Florian Postic <florian.postic@laposte.net>
2016-06-07 15:08:59 +0200

    suppression du type Queue dans le constructeur de simulateur

--- src/simulateur/Simulateur.java
+++ src/simulateur/Simulateur.java
@@ -33,8 +33,8 @@ public class Simulateur implements ISimulateur {
      */
     public Simulateur(int tFin) {
         ComparateurTExec cT = new ComparateurTExec();
-        Queue<IEvenement> fileAttente = new PriorityQueue<IEvenement>(1, cT);
-        Queue<IEvenement> fileFini = new LinkedList<IEvenement>();
+        fileAttente = new PriorityQueue<IEvenement>(1, cT);
+        fileFini = new LinkedList<IEvenement>();
         tCourant = 0;
         this.tFin = tFin;
     }
