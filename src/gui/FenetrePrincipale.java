fbd3aa01c240b3ec88090a5ed35bf101b93d062b
trillejs <trillejs@gmail.com>
2016-06-07 17:09:44 +0200

    Renommage de la classe de test du simulateur

--- src/gui/FenetrePrincipale.java
+++ src/gui/FenetrePrincipale.java
@@ -1,8 +1,14 @@
 package gui;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class FenetrePrincipale extends JFrame {
+	
+	private static GridBagConstraints contraintes;
 
 	public FenetrePrincipale() {
 		setTitle("Simulateur de réseau ad'hoc");
@@ -14,6 +20,25 @@ public class FenetrePrincipale extends JFrame {
 	public static void main(String[] args) {
 		JFrame f = new FenetrePrincipale();
 		f.setVisible(true);
+		contraintes = new GridBagConstraints();
+		
+		JPanel panneauSimulation = panneauSimulationGenerateur(); //Panneau affiché quand on est en mode simulation
+		JPanel panneauPrincipalEvenements = new JPanel(new GridBagLayout()); //Panneau affiché en mode de création des évènements
+		JPanel panneauListeNoeuds = new JPanel(new GridBagLayout());
+		
+		
+		panneauSimulation.add(panneauListeNoeuds);
+		
+		f.add(panneauSimulation);
+	}
+	
+	public static JPanel panneauSimulationGenerateur(){
+		JPanel ps = new JPanel(new GridBagLayout());
+		ps.setLayout(new GridBagLayout());
+		
+		
+		return ps;
+		
 	}
 
 }
