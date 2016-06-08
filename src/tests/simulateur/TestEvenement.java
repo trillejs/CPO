3fcf4d11110ccb483c3ba22aaa9349158c190ff0
trillejs <trillejs@gmail.com>
2016-06-08 17:01:40 +0200

    Changements tests et gui

--- src/tests/simulateur/TestEvenement.java
+++ src/tests/simulateur/TestEvenement.java
@@ -2,24 +2,35 @@ package tests.simulateur;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import reseau.OpFinEvoi;
+import reseau.OpRecevoir;
 import simulateur.*;
 
 public class TestEvenement {
-
-	private IOperation []tabOp;
-	private IEvenement []tabEv;
+	
+	private int date;
 	private ISimulateur sim;
+	private IEvenement []tabEv;
+	private IOperation []tabOp;
+	private IOperation []tabOpRecevoir;
 
-	public void setup(){
+	@Before
+	public void setUp(){
+		
+		sim = new Simulateur(60);
 		tabOp = new IOperation[20];
+		tabOpRecevoir = new IOperation[20];
 		tabEv = new IEvenement[20];
+		
 
 		for(int i=0 ; i<tabEv.length ; i++){
+			date = i;
 			tabOp[i] = new OpFinEvoi();
-			tabEv[i] = new Evenement(i, tabOp[i]);
+			tabOpRecevoir[i] = new OpRecevoir(null);
+			tabEv[i] = new Evenement(date, tabOpRecevoir[i]);
 		}
 	}
 
@@ -28,7 +39,7 @@ public class TestEvenement {
 	public void testAccesseurs() {
 		//Test getOperation
 		for(int i=0 ; i<tabEv.length ; i++){
-			assertEquals(tabEv[i].getOperation(), tabOp[i]);
+			assertEquals(tabEv[i].getOperation(), tabOpRecevoir[i]);
 		}
 
 		//Test getTExec()
@@ -39,7 +50,12 @@ public class TestEvenement {
 	
 	@Test
 	public void testSeProduire(){
-		
+		for(int i=0 ; i<tabEv.length ; i++){
+			tabEv[i].seProduire(sim);
+			
+			assertEquals(sim.getFileAttente().peek().getTExec(),tabEv[i].getTExec());
+			assertEquals(sim.getFileAttente().poll().getOperation().getClass(), tabEv[i].getOperation().getClass());
+		}
 	}
 
 }
