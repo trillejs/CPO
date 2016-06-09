558b8d24fa8175fa579b2f4ec04f667bc1c839d5
AlbaVenzal <alba.venzal@gmail.com>
2016-06-09 15:05:09 +0200

    Ajout du panneau pour le modèle de mobilité random waypoint

---
+++ src/gui/PanneauModeleRandomWaypoint.java
@@ -0,0 +1,12 @@
+package gui;
+
+import java.awt.GridLayout;
+
+public class PanneauModeleRandomWaypoint extends PanneauModeleDeMobilite {
+
+	public PanneauModeleRandomWaypoint()
+	{
+		this.setLayout(new GridLayout());
+	}
+	
+}
