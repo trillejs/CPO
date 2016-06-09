9ebe7fad1d46de03ee529fec5e0f6310cc232b6d
AlbaVenzal <alba.venzal@gmail.com>
2016-06-09 11:49:39 +0200

    Création d'un panneau pour gérer les champs du modèle de mobilité déterministe

---
+++ src/gui/PanneauModeleDeterministe.java
@@ -0,0 +1,15 @@
+package gui;
+
+import java.awt.GridLayout;
+
+import javax.swing.JPanel;
+
+public class PanneauModeleDeterministe extends JPanel{
+
+	public PanneauModeleDeterministe()
+	{
+		this.setLayout(new GridLayout());
+		
+		//
+	}
+}
