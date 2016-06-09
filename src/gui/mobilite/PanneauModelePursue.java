ec27b01a4b5bccc6fd55e6d65651fbc9ddf0b222
AlbaVenzal <alba.venzal@gmail.com>
2016-06-09 20:50:42 +0200

    Ajout de la position au panneau pursue

--- src/gui/mobilite/PanneauModelePursue.java
+++ src/gui/mobilite/PanneauModelePursue.java
@@ -1,16 +1,19 @@
 package gui.mobilite;
 
 
+import gui.PanneauPosition;
+
 import java.awt.GridLayout;
 
 import javax.swing.JButton;
+import javax.swing.text.Position;
 
 public class PanneauModelePursue extends PanneauModeleDeMobilite {
 
 	public PanneauModelePursue()
 	{
 		this.setLayout(new GridLayout());
-		this.add(new JButton("testPursue"));
+		this.add(new PanneauPosition());
 	}
 	
 }
