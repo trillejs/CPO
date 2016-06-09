0e33c3b06a26d4384ce47f77ebf7d2922ee707d2
AlbaVenzal <alba.venzal@gmail.com>
2016-06-09 20:51:59 +0200

    Hérite maintenant de RandomWalk puisqu'ils ont exactement les mêmes champs

--- src/gui/mobilite/PanneauModeleRandomWaypoint.java
+++ src/gui/mobilite/PanneauModeleRandomWaypoint.java
@@ -7,12 +7,7 @@ import java.awt.GridLayout;
 
 import javax.swing.JButton;
 
-public class PanneauModeleRandomWaypoint extends PanneauModeleDeMobilite {
+public class PanneauModeleRandomWaypoint extends PanneauModeleRandomWalk {
 
-	public PanneauModeleRandomWaypoint()
-	{
-		this.setLayout(new GridLayout());
-		this.add(new JButton("testWaypoint"));
-	}
 	
 }
