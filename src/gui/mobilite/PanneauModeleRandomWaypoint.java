5fb280ee97892aa1e6ebaade31220802a9d05892
AlbaVenzal <alba.venzal@gmail.com>
2016-06-09 23:31:35 +0200

    Ajout de getModele

--- src/gui/mobilite/PanneauModeleRandomWaypoint.java
+++ src/gui/mobilite/PanneauModeleRandomWaypoint.java
@@ -1,13 +1,18 @@
 package gui.mobilite;
 
-
-
-
-import java.awt.GridLayout;
-
-import javax.swing.JButton;
+import mobilite.ModeleDeMobilite;
+import mobilite.RandomWalk;
+import mobilite.RandomWayPoint;
 
 public class PanneauModeleRandomWaypoint extends PanneauModeleRandomWalk {
 
-	
+	@Override
+	public ModeleDeMobilite getModele() {
+		
+		int dd = Integer.parseInt(this.distanceMin.getText());
+		int df = Integer.parseInt(this.distanceMax.getText());
+		int vd = Integer.parseInt(this.vitesseMin.getText());
+		int vf = Integer.parseInt(this.vitesseMax.getText());
+		return new RandomWayPoint(dd, df, vd, vf, positionOrigin);
+	}	
 }
