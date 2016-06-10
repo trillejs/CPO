76e0ea07a5f19146aa90562b663d1c095f9f944a
AlbaVenzal <alba.venzal@gmail.com>
2016-06-10 16:06:39 +0200

    Ajout des messages d'erreurs

--- src/gui/mobilite/PanneauModeleRandomWaypoint.java
+++ src/gui/mobilite/PanneauModeleRandomWaypoint.java
@@ -1,5 +1,8 @@
 package gui.mobilite;
 
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 import gui.FenetrePrincipale;
 import mobilite.ModeleDeMobilite;
 import mobilite.RandomWayPoint;
@@ -16,11 +19,18 @@ public class PanneauModeleRandomWaypoint extends PanneauModeleRandomWalk {
 	
 	@Override
 	public ModeleDeMobilite getModele() {
-
-		int dd = Integer.parseInt(this.distanceMin.getText());
-		int df = Integer.parseInt(this.distanceMax.getText());
-		int vd = Integer.parseInt(this.vitesseMin.getText());
-		int vf = Integer.parseInt(this.vitesseMax.getText());
-		return new RandomWayPoint(dd, df, vd, vf, positionOrigin);
+		ModeleDeMobilite modele = null;
+		try{
+			int dd = Integer.parseInt(this.distanceMin.getText());
+			int df = Integer.parseInt(this.distanceMax.getText());
+			int vd = Integer.parseInt(this.vitesseMin.getText());
+			int vf = Integer.parseInt(this.vitesseMax.getText());
+			modele = new RandomWayPoint(dd, df, vd, vf, positionOrigin);
+		}
+		catch(NumberFormatException n)
+		{
+			JOptionPane.showMessageDialog(new JFrame(), "Une des valeurs n'est pas valide. Veuillez spécifier des valeurs valides");
+		}
+		return modele;
 	}	
 }
