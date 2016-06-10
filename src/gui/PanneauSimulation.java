0699a0be52f17abf98831da17334209adcb28910
AlbaVenzal <alba.venzal@gmail.com>
2016-06-10 17:14:17 +0200

    Transformé JPanel panneauListeNoeud en un nouveau type PanneauListeNoeud

--- src/gui/PanneauSimulation.java
+++ src/gui/PanneauSimulation.java
@@ -1,54 +1,26 @@
 package gui;
 
-import java.awt.Color;
+
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JList;
 import javax.swing.JPanel;
+
+import reseau.Reseau;
 
 public class PanneauSimulation extends JPanel {
 
 	private static GridBagConstraints contraintes;
-
-	/** Fenetre principale de l'application, ayant accès au réseau*/
-	private FenetrePrincipale fenetre;
 	
-	public PanneauSimulation(FenetrePrincipale fenetre)
+	public PanneauSimulation()
 	{
-		this.fenetre = fenetre;
 		this.setLayout(new GridBagLayout());
 		//Initialisations
-		JPanel panneauListeNoeuds = new JPanel(new GridBagLayout());
+		PanneauListeNoeud panneauListeNoeuds = new PanneauListeNoeud();
+		Reseau reseau = (Reseau)FenetrePrincipale.getReseau();
+		reseau.addObserver(panneauListeNoeuds);
 		JPanel panneauDetailNoeud = new JPanel(new GridBagLayout());
-		JList listeNoeuds = new JList();
-		JButton ajoutNoeud = new JButton("Ajouter noeud");
-		ajoutNoeud.addActionListener(new ActionAjouterNoeud());
-		contraintes = new GridBagConstraints();
-
-		panneauListeNoeuds.setLayout(new GridBagLayout());
-
-		//Ajout des composants
-
-		contraintes.fill = GridBagConstraints.VERTICAL;
-		contraintes.anchor = GridBagConstraints.PAGE_START;
-		contraintes.weighty = 0.20;
-		contraintes.weightx = 1;
-		contraintes.gridx = 0;
-		contraintes.gridy = 0;
-		panneauListeNoeuds.add(ajoutNoeud, contraintes);
-
-		listeNoeuds.setBackground(new Color(124));
-		contraintes.weighty = 0.80;
-		contraintes.weightx = 1;
-		contraintes.gridx = 0;
-		contraintes.gridy = 1;
-		panneauListeNoeuds.add(listeNoeuds, contraintes);
+		
 
 		contraintes.fill = GridBagConstraints.HORIZONTAL;
 		contraintes.weighty = 1;		
@@ -63,20 +35,5 @@ public class PanneauSimulation extends JPanel {
 		contraintes.gridy = 0;
 		this.add(panneauDetailNoeud, contraintes);
 
-	}
-
-	
-	class ActionAjouterNoeud implements ActionListener{
-		public void actionPerformed(ActionEvent ev)
-		{
-			//ouvre une pop up
-			JFrame frame = new JFrame();
-			frame.setSize(800,500);
-			frame.setTitle("Ajout d'un noeud");
-			frame.setLayout(new GridLayout());
-			frame.add(new PanneauAjoutNoeud(fenetre));
-			frame.setVisible(true);
-		}
-	}
-	
+	}	
 }
