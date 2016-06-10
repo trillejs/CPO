b8290080af4cad90e27abc9cde073b7f1c27f562
AlbaVenzal <alba.venzal@gmail.com>
2016-06-10 10:52:31 +0200

    Ajout de la position courant dans le constructeur

--- src/mobilite/RandomWayPoint.java
+++ src/mobilite/RandomWayPoint.java
@@ -4,9 +4,6 @@ import java.awt.geom.Point2D;
 
 public class RandomWayPoint extends RandomWalk {
 	
-	public double delai;
-	public Point2D.Double position;
-	
 	/**RandomwayPoint
 	 * 
 	 * @param dd
@@ -16,24 +13,20 @@ public class RandomWayPoint extends RandomWalk {
 	 * @param delai
 	 * @param position
 	 */
-	public RandomWayPoint(int dd, int df, int vd, int vf, double delai, Point2D.Double position) {
-		super(dd, df, vd, vf);
-		this.delai = delai;
-		this.position = position;
+	public RandomWayPoint(int dd, int df, int vd, int vf, Point2D.Double position) {
+		super(dd, df, vd, vf, position);
 	}
+	
 	/**seDeplacer
 	 * 
 	 */
 	public Point2D.Double seDeplacer(){
 		double i;
-		i = 1 + (int)(Math.random() * (20 - 1)) + 1;
-		if(i == this.delai){
-			this.position.x = this.position.x + this.distance_debut + (int)(Math.random() * ((this.distance_fin - this.distance_debut) + 1))/this.vitesse_debut + (int)(Math.random() * ((this.vitesse_fin - this.vitesse_debut) + 1));
-		
-			this.position.y = this.position.y + this.distance_debut + (int)(Math.random() * ((this.distance_fin - this.distance_debut) + 1))/this.vitesse_debut + (int)(Math.random() * ((this.vitesse_fin - this.vitesse_debut) + 1));	
-			
-			delai++;
-		}
+		i = 2 + (int)(Math.random() * (19));
+		double ajout = this.distance_debut + (int)(Math.random() * ((this.distance_fin - this.distance_debut) + 1))/this.vitesse_debut + (int)(Math.random() * ((this.vitesse_fin - this.vitesse_debut) + 1));
+		this.position.x = this.position.x + ajout;
+		this.position.y = this.position.y + ajout;
+
 		return new Point2D.Double(this.position.x, this.position.y);
 	}
 
