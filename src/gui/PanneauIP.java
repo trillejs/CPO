71505445f1c741e163ec14b1b2f399da856ae2d1
AlbaVenzal <alba.venzal@gmail.com>
2016-06-10 16:34:52 +0200

    Correction du message d'erreur

--- src/gui/PanneauIP.java
+++ src/gui/PanneauIP.java
@@ -36,21 +36,20 @@ public class PanneauIP extends JPanel {
 			for(int i = 0; i< nbChampsIp; i++)
 			{
 				tabInt[i] = Integer.parseInt(ipTab[i].getText());
-			}			
+			}
+			//On vérifie que les valeurs sont bien comprises entre 0 et 255
+			if(tabInt[0] > 0 && tabInt[0] < 256 && tabInt[1] > 0 && tabInt[1] < 256 && tabInt[2] > 0 && tabInt[2] < 256 && tabInt[3] > 0 && tabInt[3] < 256)
+			{
+				adresse = new AdresseIP(tabInt[0], tabInt[1], tabInt[2], tabInt[3]);
+			}
+			else
+			{
+				JOptionPane.showMessageDialog(new JFrame(), "Une des valeurs n'est pas valide. Veuillez spécifier des valeurs comprises entre 1 et 255");
+			}
 		}
 		catch(NumberFormatException n)
 		{
 			JOptionPane.showMessageDialog(new JFrame(), "Une des valeurs n'est pas valide. Veuillez spécifier des valeurs valides entières");
-		}
-		
-		//On vérifie que les valeurs sont bien comprises entre 0 et 255
-		if(tabInt[0] > 0 && tabInt[0] < 256 && tabInt[1] > 0 && tabInt[1] < 256 && tabInt[2] > 0 && tabInt[2] < 256 && tabInt[3] > 0 && tabInt[3] < 256)
-		{
-			adresse = new AdresseIP(tabInt[0], tabInt[1], tabInt[2], tabInt[3]);
-		}
-		else
-		{
-			JOptionPane.showMessageDialog(new JFrame(), "Une des valeurs n'est pas valide. Veuillez spécifier des valeurs comprises entre 1 et 255");
 		}
 		return adresse;
 	}
