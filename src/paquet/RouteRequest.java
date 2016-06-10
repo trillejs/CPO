2136dcea38870072b5f546a70292cba6b9d3313a
Florian Postic <florian.postic@laposte.net>
2016-06-10 10:31:55 +0200

    ajustement du à la suppression de la destination dans Paquet

--- src/paquet/RouteRequest.java
+++ src/paquet/RouteRequest.java
@@ -3,9 +3,14 @@ package paquet;
 import noeud.*;
 import reseau.IOpVisiteur;
 
+import java.util.Random;
+
 public class RouteRequest extends Paquet {
 
-	/** chemin emprunté par le routeRequest */
+	/** noeud que l'on cerche à atteindre */
+    private AdresseIP destination;
+
+	/** chemin emprunté par le routeRequest précédent */
 	private Chemin routeReply;
 	
 	/** identifiant du Route Request */
@@ -15,35 +20,33 @@ public class RouteRequest extends Paquet {
 	 * Constructeur dans le cas où le RouteRequest n'est pas en réponse à un RouteRequest
      * @param source AdresseIP du noeud source
      * @param destination AdresseIP du noeud destination
-     * @param chemin chemin parcouru par le RouteRequest pour le moment
-	 * @param identifiant du Route Request
-     * Précondition : 0 < identifiant < 255 && source != null && destination != null && chemin != null && routeReply != null
+     * @param cheminDestination chemin parcouru par le RouteRequest pour le moment
+     * Précondition : source != null && destination != null && chemin != null && routeReply != null
 	 */
-    public RouteRequest(AdresseIP source, AdresseIP destination, Chemin chemin, int identifiant) {
-    	super(source, destination, chemin);
+    public RouteRequest(AdresseIP source, AdresseIP destination, Chemin cheminDestination) {
+    	super(source, cheminDestination);
     	this.routeReply = null;
-    	this.identifiant = identifiant;
+		Random rand = new Random();
+		identifiant = rand.nextInt();
     }
 
     /**
      * Constructeur dans le cas où le RouteRequest est en réponse à un RouteRequest
      * @param source AdresseIP du noeud source
      * @param destination AdresseIP du noeud destination
-     * @param chemin chemin parcouru par le RouteRequest pour le moment
+     * @param cheminDestination chemin parcouru par le RouteRequest pour le moment
      * @param routeReply chemin emprunté par le routeRequest
-     * @param identifiant du Route Request
-     * Précondition : 0 < identifiant < 255 && source != null && destination != null && chemin != null && routeReply != null
+     * Précondition : source != null && destination != null && chemin != null && routeReply != null
      */
-    public RouteRequest(AdresseIP source, AdresseIP destination, Chemin chemin, Chemin routeReply, int identifiant)
+    public RouteRequest(AdresseIP source, AdresseIP destination, Chemin cheminDestination, Chemin routeReply)
     {
-    	super(source, destination, chemin);
+    	super(source, cheminDestination);
+
     	this.routeReply = routeReply;
-    	this.identifiant = identifiant;    	
+		Random rand = new Random();
+		identifiant = rand.nextInt();
     }
 
-	public RouteRequest(AdresseIP source, AdresseIP destination, Chemin routeReply){
-
-	}
 
     /**
      * Retourne l'identifiant de la demande de route
@@ -60,6 +63,16 @@ public class RouteRequest extends Paquet {
 		taille+=this.routeReply.getTaille();
 		taille += 1; //On ajoute la taille de l'identifiant
 		return taille;
+	}
+
+	/**
+	 * Retourne l'adresse IP destination du paquet
+	 *
+	 * @return
+	 */
+	@Override
+	public AdresseIP getDestination() {
+		return this.destination;
 	}
 
 	@Override
