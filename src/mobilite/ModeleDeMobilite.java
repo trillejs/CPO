db408b610bdf3a92a7784b72523b514195752bc2
salimtaabani <salimtaabani@yahoo.com>
2016-06-13 16:17:41 +0200

    fd

--- src/mobilite/ModeleDeMobilite.java
+++ src/mobilite/ModeleDeMobilite.java
@@ -6,14 +6,15 @@ public interface ModeleDeMobilite {
 	
     /**
      * Déplace le Noeud à une nouvelle position
-     * @return la nouvelle position du noeud après le déplacement
      */
 
-    public Point2D.Double seDeplacer();
+    public void seDeplacer();
     
-    /**getPosition
-     * renvoie la position courante du noeud
-     * @return la position du noeud
+    /** 
+     * getPosition
+     * @return la position courante du noeud
      */
+    
     public Point2D.Double getPosition();
+
 }
