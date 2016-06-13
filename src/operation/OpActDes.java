af8bb87b290d82b91529df37e2e200ce545d30f7
Florian Postic <florian.postic@laposte.net>
2016-06-13 11:09:00 +0200

    creation opActDes

---
+++ src/operation/OpActDes.java
@@ -0,0 +1,26 @@
+package operation;
+
+import simulateur.IOperation;
+import simulateur.ISimulateur;
+
+/**
+ * Created by Florian Postic (1A) on 6/13/16.
+ */
+public class OpActDes implements IOperation {
+
+    public OpActDes() {
+        // TODO
+    }
+
+    /**
+     * executer
+     * Execute les intructions composant l'opération
+     *
+     * @param simulateur - Simulateur : Simulateur
+     * @param date       - int : Date d'execution de l'opération
+     */
+    @Override
+    public void executer(ISimulateur simulateur, int date) {
+//TODO
+    }
+}
