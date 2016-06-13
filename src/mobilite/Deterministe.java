09b451ad3978d66349560b8477071b91d1758c1f
trillejs <trillejs@gmail.com>
2016-06-13 16:56:03 +0200

    Ajout du correctif à déterministe (again...) La méthode seDeplacer des classes RandomWalk/Waypoint ne fonctionnent pas.

--- src/mobilite/Deterministe.java
+++ src/mobilite/Deterministe.java
@@ -7,28 +7,28 @@ public class Deterministe implements ModeleDeMobilite {
 	/** Vitesse de déplacement
 	 */
 	private double vitesse;
-	
+
 	/** Destination que le noeud veut atteindre
 	 */
-    private Point2D.Double destination;
-    
-    /** Position courante du noeud à déplacer
-     */
-    public Point2D.Double position;
+	private Point2D.Double destination;
 
-    /**
-     * Crée un modèle de mobilité déterministe à partie d'une vitesse et d'une destination
-     * @param vitesse : double - vitesse de déplacement
-     * @param destination : Point2D.Double - destination que le noeud veut atteindre
-     * @param position_Courante : Point2D.Double - position courante du noeud à déplacer
-     */
-    public Deterministe(double vitesse, Point2D.Double destination, Point2D.Double position_Courante) {
-    	
-    	this.vitesse = vitesse;
-    	this.destination = destination;
-    	this.position = position_Courante;
-    }
-    
+	/** Position courante du noeud à déplacer
+	 */
+	public Point2D.Double position;
+
+	/**
+	 * Crée un modèle de mobilité déterministe à partie d'une vitesse et d'une destination
+	 * @param vitesse : double - vitesse de déplacement
+	 * @param destination : Point2D.Double - destination que le noeud veut atteindre
+	 * @param position_Courante : Point2D.Double - position courante du noeud à déplacer
+	 */
+	public Deterministe(double vitesse, Point2D.Double destination, Point2D.Double position_Courante) {
+
+		this.vitesse = vitesse;
+		this.destination = destination;
+		this.position = position_Courante;
+	}
+
 	/** seDeplacer
 	 * Déplace un Noeud dans le plan selon les positions du modèle
 	 * Pré : rien
@@ -42,7 +42,16 @@ public class Deterministe implements ModeleDeMobilite {
 		if(this.position.y != this.destination.y){
 			this.position.y = this.position.y + Math.sin(Math.atan(this.destination.y/this.destination.x))/this.vitesse;	
 		}
-	     }
+
+		//Cas de dépassements
+		if(this.position.x > this.destination.x){
+			this.position.x = this.destination.x;
+		}
+
+		if(this.position.y > this.destination.y){
+			this.position.y = this.destination.y;
+		}
+	}
 
 	/** getPosition
 	 * @return la position courante du noeud
@@ -50,7 +59,7 @@ public class Deterministe implements ModeleDeMobilite {
 	public Point2D.Double getPosition() {
 		return this.position;
 	}
-	
+
 	/** getDestination
 	 * @return la destination du noeud
 	 */
