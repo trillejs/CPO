a331f4e74726b1bf9612e889a2082b69e5612ef8
Florian Postic <florian.postic@laposte.net>
2016-06-13 19:05:39 +0200

    traitement du "pas de route dans la table de routage"

--- src/operation/OpEnvoyer.java
+++ src/operation/OpEnvoyer.java
@@ -1,12 +1,16 @@
 package operation;
 
 import noeud.AdresseIP;
+import noeud.Chemin;
 import noeud.INoeud;
 import paquet.Paquet;
+import paquet.RouteRequest;
 import reseau.IReseau;
 import simulateur.Evenement;
 import simulateur.ISimulateur;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 /**OpEnvoyer
@@ -38,15 +42,43 @@ public class OpEnvoyer extends OpAbstraite{
      */
     @Override
     public void executer(ISimulateur simulateur, int date) {
-        for (Map.Entry<AdresseIP, INoeud> entry:reseau.getListeNoeuds().entrySet())
-        {
-            /* Le noeud est atteignable */
-            if(reseau.atteignable(source, entry.getValue().getAdresseIP())){
-                simulateur.enregistrer(new Evenement(simulateur.gettCourant()+(paquet.getTaille()*reseau.getListeNoeuds().get(source).getDebitEmission()),new OpFinEnvoi(paquet)));
-            }else{ // Le noeud n'est pas atteignable
-                /*Envoyer un message d'erreur */
 
+        Chemin destination = paquet.getChemin();
+        /* Si le paquet ne définit pas de destination */
+        if(destination == null) {
+            /* Stocker le paquet dans la liste des paquets en attente*/
+            reseau.getListeNoeuds().get(source).ajouterPaquet(paquet);
+            /* Créer un nouveau route request */
+            RouteRequest routeRequest new RouteRequest(paquet.getSource(), paquet.getDestination(),null)
+            /* Ajouter ce route request à la liste des route request en attente*/
+            reseau.getListeNoeuds().get(source).ajouterRouteRequest(routeRequest);
+            for (Map.Entry<AdresseIP, INoeud> entry:reseau.getListeNoeuds().entrySet()) {
+                /* Le noeud est atteignable */
+                if (reseau.atteignable(source, entry.getValue().getAdresseIP())) {
+                    simulateur.enregistrer(new Evenement(simulateur.gettCourant() + (routeRequest.getTaille() * reseau.getListeNoeuds().get(source).getDebitEmission()), new OpFinEnvoi(routeRequest)));
+                    //} else { // Le noeud n'est pas atteignable
+                 /* Envoyer un message d'erreur */
+                    // Non implementé
+                    //}
+                }
+            }
+            }else{
+                /* Si le paquet définit une destination */
+                List<AdresseIP> chemin = new ArrayList<AdresseIP>(destination.getListeNoeud().entrySet());
+                /*Le noeud est atteignable*/
+                if(reseau.atteignable(source, chemin.get(0))){
+                    simulateur.enregistrer(new Evenement(simulateur.gettCourant() + paquet.getTaille() * reseau.getListeNoeuds().get(source).getDebitEmission(), new OpFinEnvoi(paquet)));
+                }//else{ // Le noeud n'est pas atteignable
+                /* Envoyer message d'erreur */
+                // Non implementé
+                //}
             }
         }
     }
 }
+
+
+/* Analyse du paquet
+*
+* */
+
