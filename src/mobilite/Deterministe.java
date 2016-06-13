c870cf039acd0e6110857ea42deb84577a9be87f
salimtaabani <salimtaabani@yahoo.com>
2016-06-13 16:11:40 +0200

    mon code jusqu'à aujourd'hui

--- src/mobilite/Deterministe.java
+++ src/mobilite/Deterministe.java
@@ -1,62 +1,66 @@
 package mobilite;
 
-
 import java.awt.geom.Point2D;
-import java.awt.geom.Point2D.Double;
 
 public class Deterministe implements ModeleDeMobilite {
 
-
-	private double vitesse;
-	private Point2D.Double destination;
-	private Point2D.Double position;
-
-	/**
-	 * Crée un modèle de mobilité déterministe à partie d'une vitesse et d'une destination
-	 * @param vitesse de déplacement
-	 * @param destination à atteindre
+	/** Vitesse de déplacement
 	 */
-	public Deterministe(double vitesse, Point2D.Double destination, Point2D.Double position_Courante) {
+	private double vitesse;
+	
+	/** Destination que le noeud veut atteindre
+	 */
+    private Point2D.Double destination;
+    
+    /** Position courante du noeud à déplacer
+     */
+    public Point2D.Double position;
 
-		this.vitesse = vitesse;
-		this.destination = destination;
-		this.position = position_Courante;
-	}
-
+    /**
+     * Crée un modèle de mobilité déterministe à partie d'une vitesse et d'une destination
+     * @param vitesse : double - vitesse de déplacement
+     * @param destination : Point2D.Double - destination que le noeud veut atteindre
+     * @param position_Courante : Point2D.Double - position courante du noeud à déplacer
+     */
+    public Deterministe(double vitesse, Point2D.Double destination, Point2D.Double position_Courante) {
+    	
+    	this.vitesse = vitesse;
+    	this.destination = destination;
+    	this.position = position_Courante;
+    }
+    
 	/** seDeplacer
 	 * Déplace un Noeud dans le plan selon les positions du modèle
 	 * Pré : rien
 	 * Post : rien
-	 * @return Point2D.Double la nouvelle position du noeud
 	 */
-	@Override
-	public Point2D.Double seDeplacer() {
+
+	public void seDeplacer() {
 		if(this.position.x != this.destination.x){
-			this.position.x = this.position.x + Math.cos(Math.atan(this.destination.y/this.destination.x));
+			this.position.x = this.position.x + Math.cos(Math.atan(this.destination.y/this.destination.x))/this.vitesse;
 		}
 		if(this.position.y != this.destination.y){
-			this.position.y = this.position.y + Math.sin(Math.atan(this.destination.y/this.destination.x));	
+			this.position.y = this.position.y + Math.sin(Math.atan(this.destination.y/this.destination.x))/this.vitesse;	
 		}
-
-		//Cas de dépassements
-		if(this.position.x > this.destination.x){
-			this.position.x = this.destination.x;
-		}
-
-		if(this.position.y > this.destination.y){
-			this.position.y = this.destination.y;
-		}
-		return new Point2D.Double(this.position.x, this.position.y);
-	}
+	     }
 
 	/** getPosition
-	 * 
+	 * @return la position courante du noeud
 	 */
 	public Point2D.Double getPosition() {
 		return this.position;
 	}
 	
+	/** getDestination
+	 * @return la destination du noeud
+	 */
 	public Point2D.Double getDestination(){
 		return this.destination;
+	}
+	/**getVitesse
+	 * @return la vitesse du noeud
+	 */
+	public double getVitesse(){
+		return this.vitesse;
 	}
 }
